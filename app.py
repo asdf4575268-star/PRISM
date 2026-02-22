54bd9cffbb0d51de95ff182716bc41fce7d714f6
asdf4575268-star <asdf4575268@gmail.com>
2026-02-22 13:44:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -405,7 +405,7 @@ with tab1:
                     detail_url = f"https://www.kopis.or.kr/por/db/pblprfr/pblprfrView.do?menuId=MNU_00020&mt20Id={s['id']}"
                     st.session_state.api_data = {
                         'title': f"{s['title']} ",
-                        'creator': combined_creator, # 상세 정보에서 가져온 제작진/출연진
+                        'creator': combined_creator_with_venue, # 상세 정보에서 가져온 제작진/출연진
                         'date': f"{s['date']}",
                         'img': s['img'],
                         'summary': detail_url
@@ -649,6 +649,7 @@ with tab2:
                                     show_details(row)
             else: 
                 st.info(f"{c_name} 기록이 아직 없습니다.")
+
 
 
 
