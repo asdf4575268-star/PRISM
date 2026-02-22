d4dd093bbde20477307d4ce32ef56286988e781f
asdf4575268-star <asdf4575268@gmail.com>
2026-02-22 12:38:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -392,21 +392,15 @@ with tab1:
                 
                 if st.button("✨ 가져오기"):
                     s = opts[sel]
-                    
-                    # 상세 정보(제작/출연) 가져오기
                     combined_creator = get_kopis_detail(s['id'])
-                    # 상세 페이지 URL
                     detail_url = f"https://www.kopis.or.kr/por/db/pblprfr/pblprfrView.do?menuId=MNU_00020&mt20Id={s['id']}"
-                    
                     st.session_state.api_data = {
                         'title': f"{s['title']}",
-                        'creator': combined_creator, 
-                        'date': s['date'],
-                        'venue': s['venue'],
+                        'creator': combined_creator, # 상세 정보에서 가져온 제작진/출연진
+                        'date': s['date']@{s['venue']},
                         'img': s['img'],
                         'summary': detail_url
                     }
-                    st.rerun()
                     
                     st.rerun()
         else: # MOVIES, SERIES
@@ -646,8 +640,4 @@ with tab2:
                                     show_details(row)
             else: 
                 st.info(f"{c_name} 기록이 아직 없습니다.")
-
-
-
-
 
