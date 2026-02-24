8553ffe2b267e790463f9dd39ccf37f7b4cee276
asdf4575268-star <asdf4575268@gmail.com>
2026-02-24 11:42:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -445,6 +445,7 @@ with tab_a:
                                 with cols[j]:
                                     st.markdown(f'<div class="cal-img-box {tab_cls}"><div class="badge-cat">{row["category"]}</div><div class="badge-date">{row["view_date"]}</div><img src="{row["img_url"]}" {img_style}></div>', unsafe_allow_html=True)
                                     if st.button(row['title'][:10], key=f"all_btn_{row['id']}", use_container_width=True): show_details(row)
+                                        show_details(row)
 
         # --- [카테고리 탭] ---
         for idx, c_name in enumerate(cat_order):
@@ -470,6 +471,7 @@ with tab_a:
                                         </div>
                                     ''', unsafe_allow_html=True)
                                     if st.button(row['title'][:10], key=f"cat_btn_{c_name}_{row['id']}", use_container_width=True): show_details(row)
+
 
 
 
