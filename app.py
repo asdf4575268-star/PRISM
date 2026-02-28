c9b52c16040b21e13de534337f72af75028a7aa3
asdf4575268-star <asdf4575268@gmail.com>
2026-02-28 13:15:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -334,7 +334,6 @@ def show_details(item):
             
         with col_txt:
             st.markdown(f'# {item.get("title")}')
-            st.write(f"#### **{item.get('category')}**")
             st.write(f"**{item.get('creator')}**")
             st.write(f"**📅 {item.get('rel_date')} | 📍 {item.get('venue')}**")
             st.markdown(f'<p style="color: #E2E2E2; font-weight: bold; font-size: 1.1em;">🍿감상일: {item.get("view_date")}</p>', unsafe_allow_html=True)
@@ -522,4 +521,5 @@ with tab_a:
                                     img_u = row["img_url"] if row["img_url"] and str(row["img_url"]) != "None" else ""
                                     st.markdown(f'<div class="cal-img-box {music_cls}"><div class="badge-date">{row["view_date"]}</div><img src="{img_u}"></div>', unsafe_allow_html=True)
                                     if st.button(row['title'][:10], key=f"cat_btn_{c_name}_{row['id']}", use_container_width=True): show_details(row)
+
 
