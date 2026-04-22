b769aa88ad94c026779b7e0a4ce49892e4f1a921
asdf4575268-star <asdf4575268@gmail.com>
2026-04-22 14:40:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -510,10 +510,10 @@ if IS_ADMIN and tab_w:
 
             if is_update:
                 if cb1.button("💾 수정 내용 저장", use_container_width=True, type="primary"):
-                    if save_data(is_update=True): st.success("✅ 안전하게 수정되었습니다!"); time.sleep(0.8); st.rerun()
+                    if save_data(is_update=True): st.success("✅ 수정 완료"); time.sleep(0.8); st.rerun()
             else:
                 if cb1.button("✅ 아카이브 직접 저장", use_container_width=True, type="primary"):
-                    if save_data(to_archive=True): st.success("✅ 아카이브 저장 완료!"); time.sleep(0.8); st.rerun()
+                    if save_data(to_archive=True): st.success("✅ 저장 완료"); time.sleep(0.8); st.rerun()
                 if cb2.button("🗓️ Weekly Contents에 계획 등록", use_container_width=True):
                     if save_data(to_archive=False): st.success("🗓️ Weekly Contents에 추가되었습니다!"); time.sleep(0.8); st.rerun()
 
