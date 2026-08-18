fa44550237d216b0bb1c80a324e40c5e7811e530
asdf4575268-star <asdf4575268@gmail.com>
2026-08-18 12:52:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -367,15 +367,15 @@ def render_item_details(data_dict, item_id, is_plan=False):
                 
             with col_in2:
                 if cat == "SCRAP":
-                    e_summary = st.text_area("📰 QUOTE(url)", value=safe_str(data_dict.get('summary')), height=120)
-                    e_note = st.text_area("✍️ HANDWRITE(brief)", value=safe_str(data_dict.get('note')), height=120)
-                    e_brief = st.text_input("🎯 CONTEXT(argument)", value=safe_str(data_dict.get('brief')))
-                    e_highlights = st.text_area("💡 EXAMPLS(evidences)/STRUCTURE", value=safe_str(data_dict.get('highlights')), height=100)
+                    e_summary = st.text_area("📰 Quote", value=safe_str(data_dict.get('summary')), height=120)
+                    e_brief = st.text_input("🎯 Argument", value=safe_str(data_dict.get('brief')))
+                    e_note = st.text_area("✍️ 5BRIEF", value=safe_str(data_dict.get('note')), height=120)
+                    e_highlights = st.text_area("💡 Evidence(examples)", value=safe_str(data_dict.get('highlights')), height=100)
                 else:
                     e_brief = st.text_input("💎 DRIP", value=safe_str(data_dict.get('brief')))
-                    e_note = st.text_area("🖋️ PRISM", value=safe_str(data_dict.get('note')), height=200)
-                    e_summary = st.text_area("💡 BRIEF", value=safe_str(data_dict.get('summary')), height=100)
+                    e_note = st.text_area("💡 BRIEF", value=safe_str(data_dict.get('summary')), height=100)
                     e_highlights = st.text_area("🔖 POINT", value=safe_str(data_dict.get('highlights')), height=100)
+                    e_note = st.text_area("🖋️ PRISM", value=safe_str(data_dict.get('note')), height=200)
 
             c_save, c_cancel = st.columns([0.5, 0.5])
             if c_save.form_submit_button("💾 저장하기", type="primary", use_container_width=True):
