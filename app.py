e4f5cab77638c9c226954264f0a1d6a91b98e3e0
asdf4575268-star <asdf4575268@gmail.com>
2026-09-08 19:24:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -539,7 +539,6 @@ def render_item_details(data_dict, item_id, is_plan=False):
     col_img, col_txt = st.columns([0.3, 0.7])
     with col_img:
         if img_url and str(img_url) != "None":
-            img_col_l, img_col_c, img_col_r = st.columns([0.5, 1, 0.5])
             with img_col_c:
                 st.image(img_url, width=300)
         
