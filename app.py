7e6af1da30190ceb5901df0b656dd5371ba7fbe4
asdf4575268-star <asdf4575268@gmail.com>
2026-09-08 22:55:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -803,7 +803,7 @@ if IS_ADMIN and tab_w:
 
     st.markdown("### 📅 WEEKLY")
 
-    nav_left, nav_center, nav_right = st.columns([0.1, 0.8, 0.1])
+    nav_left, nav_center, nav_right = st.columns([0.15, 0.7, 0.15])
 
     with nav_left:
         if st.button("⬅️", use_container_width=True, key="w_prev_week"):
