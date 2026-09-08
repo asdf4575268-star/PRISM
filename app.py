339a1bb985fa204d46ed0e4a96c46384f4c4bb7d
asdf4575268-star <asdf4575268@gmail.com>
2026-09-08 20:00:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1186,7 +1186,7 @@ elif not tab_w:
     st.markdown("""<style>
     .cal-img-box { 
         position: relative; 
-        width: 60%; 
+        width: 40%; 
         aspect-ratio: 1/1.4; 
         overflow: hidden; 
         border-radius: 12px; 
