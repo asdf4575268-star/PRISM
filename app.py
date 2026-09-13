b67461c02019c7d3aa418df2656405c20fac4a42
asdf4575268-star <asdf4575268@gmail.com>
2026-09-13 22:07:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -956,7 +956,7 @@ elif not tab_w:
                     m_data = main_df[main_df['year_month'] == ym]
                     
                     # 월별 헤더 출력
-                    st.markdown(f"#### 📁 {ym.year}년 {ym.month}월 ({len(m_data)})")
+                    st.markdown(f"<h4 style='font-size: 1.2rem; margin-bottom: 10px; margin-top: 20px;'>📁 {ym.year}년 {ym.month}월 ({len(m_data)})</h4>", unsafe_allow_html=True)
                     
                     items = m_data.to_dict('records')
                     
